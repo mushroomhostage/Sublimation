a5a6c35ff81d11baaeb83a7c9e833287e733d612
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-16 12:55:26 -0800

    Cancel placing ice in the nether and play smoke

--- Sublimation.java
+++ Sublimation.java
@@ -2,16 +2,50 @@
 package com.exphc.Sublimation;
 
 import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.*;
+import org.bukkit.event.*;
+import org.bukkit.event.block.*;
+import org.bukkit.Material.*;
+import org.bukkit.entity.*;
+import org.bukkit.block.*;
+import org.bukkit.*;
+
+class SublimationListener extends BlockListener {
+    Plugin plugin;
+
+    public SublimationListener(Plugin pl) {
+        plugin = pl;
+    }
+
+    public void onBlockPlace(BlockPlaceEvent event) {
+        Block block = event.getBlockPlaced();
+        Player player = event.getPlayer();
+        World world = block.getWorld();
+
+        if (world.getEnvironment() == World.Environment.NETHER && block.getType() == Material.ICE) {
+            // sublimate
+            world.playEffect(player.getLocation(), Effect.SMOKE, 0);    
+
+            event.setCancelled(true);
+        }
+    }
+}
 
 public class Sublimation extends JavaPlugin {
     Logger log = Logger.getLogger("Minecraft");
+    BlockListener blockListener;
 
     public void onEnable() {
-        log.info("TODO: enable");
+        log.info("Enabling sublimation");
+
+        blockListener = new SublimationListener(this);
+
+        Bukkit.getPluginManager().registerEvent(org.bukkit.event.Event.Type.BLOCK_PLACE, blockListener, org.bukkit.event.Event.Priority.Highest, this);
     }
 
     public void onDisable() {
-        log.info("TODO: disable");
+        log.info("Disabling sublimation");
     }
 }
