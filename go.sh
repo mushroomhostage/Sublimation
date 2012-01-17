693414db358a83f71f76d54d7110d2d676880554
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-16 18:22:02 -0800

    Include source

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ CLASSPATH=../craftbukkit-1.0.1-R1.jar javac *.java
 rm -rf me
 mkdir -p me/exphc/Sublimation
 mv *.class me/exphc/Sublimation
-jar cf Sublimation.jar me/ *.yml
+jar cf Sublimation.jar me/ *.yml *.java
 cp Sublimation.jar ../plugins/
