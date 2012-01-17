18a1c4b159110d959d83fda3c366587b2fdcbbd2
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-16 23:04:35 -0800

    Include README in jar

--- go.sh
+++ go.sh
@@ -3,5 +3,5 @@ CLASSPATH=../craftbukkit-1.0.1-R1.jar javac *.java
 rm -rf me
 mkdir -p me/exphc/Sublimation
 mv *.class me/exphc/Sublimation
-jar cf Sublimation.jar me/ *.yml *.java
+jar cf Sublimation.jar me/ *.yml *.java README
 cp Sublimation.jar ../plugins/
