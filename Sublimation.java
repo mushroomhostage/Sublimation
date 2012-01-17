5b5882074478d133ff339953ad2131d4be8f9df9
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-16 18:16:11 -0800

    Simplify smoke on place

--- Sublimation.java
+++ Sublimation.java
@@ -12,18 +12,6 @@ import org.bukkit.entity.*;
 import org.bukkit.block.*;
 import org.bukkit.*;
 
-class SmokeTask implements Runnable {
-    Location location;
-
-    public SmokeTask(Location loc) {
-        location = loc;
-    }
-
-    public void run() {
-        location.getWorld().playEffect(location, Effect.SMOKE, 0);
-    }
-}
-
 class SublimationListener extends BlockListener {
     Plugin plugin;
 
@@ -41,16 +29,11 @@ class SublimationListener extends BlockListener {
                 // return to player
                 event.setCancelled(true); 
             } else {
-                Location location = block.getLocation();
-
-                // make air
+                // replace
                 block.setType(Material.AIR);
-
-                //SmokeTask smokeTask = new SmokeTask(location);
-                //Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, smokeTask, 0);
-
-                // sublimate
-                // TODO: delay smoke? doesn't seem to show
+            }
+            if (plugin.getConfig().getBoolean("smoke", true)) {
+                // turn into smoke
                 world.playEffect(block.getLocation(), Effect.SMOKE, 0);    
             }
         }
