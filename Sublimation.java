b4276632f1970a704c2872f4c9bef05f05c458d1
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-22 12:53:57 -0800

    Add 'collect' option, to drop ice when mined with silk touch

--- Sublimation.java
+++ Sublimation.java
@@ -10,6 +10,8 @@ import org.bukkit.event.block.*;
 import org.bukkit.Material.*;
 import org.bukkit.entity.*;
 import org.bukkit.block.*;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.*;
 
 class SublimationListener extends BlockListener {
@@ -38,6 +40,28 @@ class SublimationListener extends BlockListener {
             }
         }
     }
+
+    // drop ice
+    public void onBlockBreak(BlockBreakEvent event) {
+        Block block = event.getBlock();
+
+        if (block.getType() != Material.ICE) {
+            return;
+        }
+
+        Player player = event.getPlayer();
+        ItemStack tool = player.getItemInHand();
+
+        if (tool.containsEnchantment(Enchantment.SILK_TOUCH) && plugin.getConfig().getBoolean("collect")) {
+            World world = block.getWorld();
+            ItemStack drop = new ItemStack(block.getType(), 1);
+
+            world.dropItemNaturally(block.getLocation(), drop);
+
+            block.setType(Material.AIR);
+            // do not cancel the event, so that it still uses up tool durability
+        }
+    }
 }
 
 public class Sublimation extends JavaPlugin {
@@ -47,12 +71,15 @@ public class Sublimation extends JavaPlugin {
     public void onEnable() {
         log.info(getDescription().getFullName() + " enabled");
 
-        this.getConfig().options().copyDefaults(true);
+        getConfig().options().copyDefaults(true);
         saveConfig();
 
         blockListener = new SublimationListener(this);
 
         Bukkit.getPluginManager().registerEvent(org.bukkit.event.Event.Type.BLOCK_PLACE, blockListener, org.bukkit.event.Event.Priority.Highest, this);
+        if (getConfig().getBoolean("collect")) {
+            Bukkit.getPluginManager().registerEvent(org.bukkit.event.Event.Type.BLOCK_BREAK, blockListener, org.bukkit.event.Event.Priority.Highest, this);
+        }
     }
 
     public void onDisable() {
